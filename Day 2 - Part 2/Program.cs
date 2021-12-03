5e76143a3785fd49dbc09f9ab24a71c9bfe7ba42
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-12-03 22:54:42 +0100

    Add project files.

---
+++ Day 2 - Part 2/Program.cs
@@ -0,0 +1,27 @@
+﻿// forward x down x up x
+
+string[] input = System.IO.File.ReadAllLines("Input.txt");
+
+int depth = 0;
+int forward = 0;
+int aim = 0;
+
+foreach (var instruction in input)
+{
+    string[] splitInput = instruction.Split(' ');
+    if (splitInput[0] == "forward")
+    {
+        forward = forward + int.Parse(splitInput[1]);
+        depth = depth + (int.Parse(splitInput[1]) * aim);
+    }
+    else if (splitInput[0] == "down")
+    {
+        aim = aim + int.Parse(splitInput[1]);
+    }
+    else if (splitInput[0] == "up")
+    {
+        aim = aim - int.Parse(splitInput[1]);
+    }
+}
+int result = depth * forward;
+Console.WriteLine(result);
